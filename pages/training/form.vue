c89d8099250a03a6701c084d56783e6b77a6257a
mr cod3r <mrcod3r.ir@gmail.com>
2021-06-24 12:19:25 +0430

    validate form by veeValidate 100%

--- pages/training/form.vue
+++ pages/training/form.vue
@@ -3,30 +3,47 @@
     <h1>form</h1>
     <br />
     <h2>form validation by vee-validate</h2>
-
-    <validation-provider v-slot="{ errors }" rules="even" name="number">
-      <label>number : </label>
-      <text-input
-        v-model="numberValue"
-        :type="'number'"
-        name="number"
-      ></text-input>
-      <span>{{ errors[0] }}</span>
-    </validation-provider>
-    <br />
-    <label> firstName: </label>
-    <text-input v-model="firstName" name="firstName"></text-input>
-    <br />
-    <number-picker
-      v-model="count"
-      :min-value="4"
-      class="d-flex"
-    ></number-picker>
+    <validation-observer v-slot="{ handleSubmit, reset }">
+      <form @submit.prevent="handleSubmit(onSubmit)">
+        <validation-provider
+          v-slot="{ errors }"
+          rules="even|required"
+          name="number"
+        >
+          <label>number : </label>
+          <text-input
+            v-model="numberValue"
+            :type="'number'"
+            name="number"
+          ></text-input>
+          <span>{{ errors[0] }}</span>
+        </validation-provider>
+        <br />
+        <validation-provider v-slot="{ errors }" rules="required" name="name">
+          <label> firstName: </label>
+          <text-input v-model="firstName" name="firstName"></text-input>
+          <span v-show="errors && errors.length > 0">{{ errors[0] }}</span>
+        </validation-provider>
+        <br />
+        <number-picker
+          v-model="count"
+          :min-value="4"
+          class="d-flex"
+        ></number-picker>
+        <br />
+        <button type="submit" class="btn btn-outline-primary">
+          submit form
+        </button>
+        <button type="reset" class="btn btn-outline-secondary" @click="reset">
+          reset form
+        </button>
+      </form>
+    </validation-observer>
   </div>
 </template>
 
 <script>
-import { ValidationProvider, extend } from 'vee-validate'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
 import NumberPicker from '~/components/ui/NumberPicker.vue'
 import TextInput from '~/components/ui/TextInput.vue'
 extend('even', (value) => {
@@ -37,7 +54,8 @@ export default {
   components: {
     TextInput,
     NumberPicker,
-    ValidationProvider
+    ValidationProvider,
+    ValidationObserver
   },
   layout: 'training',
   data() {
@@ -50,6 +68,11 @@ export default {
   watch: {
     firstName(value) {
       console.log('firstName is : ' + value)
+    }
+  },
+  methods: {
+    onSubmit() {
+      alert('form submited !')
     }
   }
 }
